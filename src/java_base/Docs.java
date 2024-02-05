3407818490ed68a711fbb71a2b9297066cb9c294
Elena Khaidarova <alenakhaidgarovv@gmail.com>
2024-02-06 00:46:46 +0300

    5

---
+++ src/java_base/Docs.java
@@ -0,0 +1,20 @@
+package java_base;
+
+import java.util.Scanner;
+
+public class Docs {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] clients = {"Иван", "Петр", "Вика"};
+        String[] docs = {"паспорт", "водительское удостоверение", "свидетельство о рождении"};
+
+        System.out.println("Ввести количество доков");
+        int docsclients = sc.nextInt();
+
+        if( docsclients != docs.length){
+            System.out.println("Ошибка: неверное количество документов " );
+        } else {
+            System.out.println("документы загруженны");
+        }
+    }
+}
