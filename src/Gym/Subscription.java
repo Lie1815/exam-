e783c51de0afd6b89c6beaa17f46d35d2643ac68
Elena Khaidarova <alenakhaidgarovv@gmail.com>
2024-04-10 20:09:53 +0300

    1

--- src/Gym/Subscription.java
+++ src/Gym/Subscription.java
@@ -1,2 +1,27 @@
-package Gym;public class Subscription {
-}
+package Gym;
+import java.time.LocalDate;
+import java.util.Date;
+public class Subscription {
+        private People owner;
+        private Date registrationDate;
+        private Date expirationDate;
+
+        public Subscription(People owner, Date registrationDate, Date expirationDate) {
+            this.owner = owner;
+            this.registrationDate = registrationDate;
+            this.expirationDate = expirationDate;
+        }
+
+        public People getOwner() {
+            return owner;
+        }
+
+        public Date getRegistrationDate() {
+            return registrationDate;
+        }
+
+        public Date getExpirationDate() {
+            return expirationDate;
+        }
+    }
+
